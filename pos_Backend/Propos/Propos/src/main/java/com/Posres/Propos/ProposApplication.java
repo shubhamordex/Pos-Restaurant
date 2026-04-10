db839192bed572194a923dec9f916020d288a7ff
ashish-sonagara-ordex <shubham.ramani.ordex@gmail.com>
2026-04-10 16:02:04 +0530

    Backend
    BAckend

---
+++ pos_Backend/Propos/Propos/src/main/java/com/Posres/Propos/ProposApplication.java
@@ -0,0 +1,13 @@
+package com.Posres.Propos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProposApplication.class, args);
+	}
+
+}
---
+++ pos_Backend/Propos/Propos/src/test/java/com/Posres/Propos/ProposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Posres.Propos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
